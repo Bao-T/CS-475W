e5bf89405abec3bfe4217571895c31254c9fa09e
CyruzCampos <cyrsseth@aol.com>
2017-03-16 22:33:23 -0700

    Updated the PCB.h header file again.

--- SimProj/ConsoleApplication1/PCB.h
+++ SimProj/ConsoleApplication1/PCB.h
@@ -1,6 +1,25 @@
-#include <iostream>
-#include <vector>
+#include <iostream> 
+#include <vector> 
 
 class PCB {
+private:
+	int priority;
+	long int programCounter;
+	double timeLimit;
 
+public:
+	std::vector <double, double> cpuBurstT;
+	std::vector <double, double> ioBurstT;
+
+	int getPriority() {
+		return priority;
+	}
+
+	int getProgramCounter() {
+		return programCounter;
+	}
+
+	double getTimeLimit() {
+		return timeLimit;
+	}
 };
