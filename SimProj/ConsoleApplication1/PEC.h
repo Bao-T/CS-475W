794d3a1dbc395311f698c90fade70e611392882e
cblair-garcia18 <cblair-garcia18@my.whitworth.edu>
2017-03-17 09:43:09 -0700

    Implemented PEC header.

---
+++ SimProj/ConsoleApplication1/PEC.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class PEC {
+
+private:
+	string textregion;
+	int PID;
+	int processControlBlock;
+
+
+public:
+	enum State;
+
+	vector<int> dataRegion;
+
+
+
+	string returntextregion()
+	{
+		return textregion;
+	}
+
+	int returnPID()
+	{
+		return PID;
+	}
+
+};
